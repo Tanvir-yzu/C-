3b2591a0b37d31d0af08d28b571c418784328323
Tanvir(Software Engineer) <89591395+Tanvir-yzu@users.noreply.github.com>
2024-02-18 00:34:21 +0800

    Add files via upload

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -17,10 +17,15 @@ int main (){
   bool power = true;
   string name = "multyple";
   string day = "friday";
+  double pi = 3.1416;
+  double radius= 10;
+  double cirumference = 2 * pi * radius;
+  
 
 
 
-  cout<<  "hello  "   <<name<<", how are you today?"<<endl;
+   cout<< cirumference << "cm" << endl;
+  //cout<<  "hello  "   <<name<<", how are you today?"<<endl;
 
    return 0;
 }
