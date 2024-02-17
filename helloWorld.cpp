d75b53aa69729640a951b067ceac735953daa48b
Tanvir(Software Engineer) <89591395+Tanvir-yzu@users.noreply.github.com>
2024-02-18 01:00:35 +0800

    Add files via upload

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,31 +1,45 @@
 #include <iostream>
 using namespace std;
-int main (){
-  // cout<<"i like pizza!"<<endl;
-  /*
-  This is
-  a
-  multyple
-  commemt
-  
-  */
-  int  x =10, y=5;
-  int z = x+y;
-  double gpa = 3.5;
-  char grade = 'a';
-  bool std= false;
-  bool power = true;
-  string name = "multyple";
-  string day = "friday";
-  const double PI = 3.1416;
-  double radius= 10;
-  double cirumference = 2 * PI * radius;
-  
 
+namespace first
+{
+    int x = 10; // global variable in the first namespace
+}
+namespace second
+{
+    int x = 0;
 
+}
+int main()
+{
 
-   cout<< cirumference << "cm" << endl;
-  //cout<<  "hello  "   <<name<<", how are you today?"<<endl;
+    //using namespace first;
+    using namespace second;
 
-   return 0;
+    // cout<<"i like pizza!"<<endl;
+    /*
+    This is
+    a
+    multyple
+    commemt
+
+    */
+    // int  x =10, y=5;
+    // int z = x+y;
+    double gpa = 3.5;
+    char grade = 'a';
+    bool std = false;
+    bool power = true;
+    string name = "multyple";
+    string day = "friday";
+    const double PI = 3.1416;
+    double radius = 10;
+    double cirumference = 2 * PI * radius;
+
+    // cout<< cirumference << "cm" << endl;
+    // cout<<  "hello  "   <<name<<", how are you today?"<<endl;
+     cout<< first::x<<endl;
+    cout << x;
+
+    return 0;
 }
